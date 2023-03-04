6c0b8440c6700aa14b150daead979f37129f5bcb
Mauroo <100035249+mauroociappinaph@users.noreply.github.com>
2023-03-04 18:39:44 -0300

    JS al H2

---
+++ js/app.js
@@ -0,0 +1,7 @@
+
+/* HEADER */
+
+const element = document.querySelector(".header__texto h2");
+element.addEventListener("click", () => {
+  alert("Aprende de los mejores con las mejores recetas y consejos. Mira nuestras recetas y descubre los mejores consejos para preparar el café perfecto. ¡Empieza ahora mismo!");
+});
